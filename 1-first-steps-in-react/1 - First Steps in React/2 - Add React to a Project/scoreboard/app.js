91d79beba6b1f9e44e8c01d58ab6586f364a9b56
gerlinp <gerlin.pierrelouis@gmail.com>
2021-08-03 00:07:05 -0400

    props to create reusable components

--- 1-first-steps-in-react/1 - First Steps in React/2 - Add React to a Project/scoreboard/app.js
+++ 1-first-steps-in-react/1 - First Steps in React/2 - Add React to a Project/scoreboard/app.js
@@ -1,9 +1,51 @@
-const title =  React.createElement(
-    'h1',
-    { id: 'main-title', title: 'This is a title.' },
-    'My First React Element!'
+const Header = (props) => {
+    console.log(props)
+    return  (
+     <header>
+         <h1>{props.title}</h1>
+         <span className="stats">Players: { props.totalPlayers }</span>
+     </header>   
+    ) 
+}
+
+const Player = (props) => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                {props.name}
+            </span>
+        <Counter  score={props.score}/>
+
+        </div>
+    )
+}
+
+const Counter = (props) => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement">-</button>
+            <span className="counter-score">{props.score}</span>
+            <button className="counter-action increment">+</button>
+        </div>
+    )
+}
+
+const App = () => {
+return (
+        <div className="scoreboard">
+            <Header title="Scoreboard"
+                    totalPlayers={1} />
+
+            {/* Play list */}
+            <Player name="Gerlin" score={50}/>
+            <Player name="Michelle" score={90}/>
+            <Player name="Kirk" score={85}/>
+            <Player name="Schaffer" score={80}/>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
 );
-
-
-
-console.log(title)
